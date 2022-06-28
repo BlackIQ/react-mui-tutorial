bffe1011d0cd759331c54745303be028ec4157e1
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-28 18:15:14 +0430

    Add components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Create, Home } from '@mui/icons-material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path={'/'}>
+          <Home />
+        </Route>
+        <Route exact path={'/create'}>
+          <Create />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
