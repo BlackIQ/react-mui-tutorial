7fb433173c69d0289e83f38419566354a122ec1e
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-30 22:19:20 +0430

    Add theme

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Home from './components/home';
+import {
+  createTheme,
+  ThemeProvider
+} from "@mui/material/styles";
+
 import Create from './components/create';
+import Home from './components/home';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Arima",
+    fontWeightLight: 200,
+    fontWeightRegular: 300,
+    fontWeightMedium: 400,
+    fontWeightBold: 600
+  }
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route exact path='/create'>
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/create'>
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
