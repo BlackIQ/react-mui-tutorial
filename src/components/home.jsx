e0a8f5090c3e2c50b118520ccc0f791fceda0356
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-30 22:40:14 +0430

    Add get

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,8 +1,23 @@
+import { useState, useEffect } from "react";
+import Axios from "axios";
+
 const Home = () => {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        Axios.get('http://localhost:8000/tasks')
+            .then((result) => setTasks(result.data))
+            .catch((error) => console.log(error));
+    });
+
     return (
-        <div>   
-            Hello
-        </div>
+        tasks.map((item) => {
+            return (
+                <div>
+                    <h3>{item.name}</h3>
+                </div>
+            );
+        })
     );
 }
 
