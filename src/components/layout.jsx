7ef8805057d6845bf3fea57a2bcc083de2031867
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-01 16:25:07 +0430

    Add home button navigation stuff

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 
+import Create from './create';
+import Home from './home';
+
 import {
-    Home,
+    Home as HomeIcon,
     Add
 } from "@mui/icons-material";
 
@@ -15,10 +19,14 @@ import {
     ListItemText,
     ListItemIcon,
     ListItemButton,
-    Avatar
+    Avatar,
+    BottomNavigationAction,
+    BottomNavigation,
+    Paper
 } from "@mui/material";
 
 import { makeStyles } from "@mui/styles";
+
 import { format } from "date-fns";
 
 const drawerWidth = 240;
@@ -26,9 +34,8 @@ const drawerWidth = 240;
 const useStyles = makeStyles((theme) => {
     return {
         layout: {
-            background: "#f9f9f9",
             width: "100%",
-            padding: theme.spacing(3),
+            padding: theme.spacing(1),
         },
         drawer: {
             width: drawerWidth
@@ -46,9 +53,6 @@ const useStyles = makeStyles((theme) => {
         date: {
             flexGrow: 1
         },
-        avatar: {
-            marginLeft: theme.spacing(2)
-        }
     };
 });
 
@@ -62,7 +66,7 @@ const Layout = (props) => {
     const list = [
         {
             text: 'Tasks',
-            icon: <Home color="primary" />,
+            icon: <HomeIcon color="primary" />,
             path: '/'
         },
         {
@@ -72,16 +76,24 @@ const Layout = (props) => {
         }
     ];
 
+    const [tab, setTab] = useState(0);
+
+    const mobile = false;
+
     return (
         <div className={classes.root}>
             <AppBar
                 className={classes.appbar}
                 elevation={0}
                 osition="fixed"
-                sx={{
-                width: { sm: `calc(100% - ${drawerWidth}px)` },
-                ml: { sm: `${drawerWidth}px` },
-                }}
+                sx={
+                    !mobile
+                    &&
+                    {
+                        width: { sm: `calc(100% - ${drawerWidth}px)` },
+                        ml: { sm: `${drawerWidth}px` },
+                    }
+                }
             >
                 <Toolbar>
                     <Typography
@@ -90,45 +102,78 @@ const Layout = (props) => {
                     >
                         Today is { format(new Date(), "do MMMM Y") }
                     </Typography>
-                    <Typography>Amirhossein</Typography>
                     <Avatar className={classes.avatar} alt="amir" src="https://avatars.githubusercontent.com/u/55284339?v=4" />
                 </Toolbar>
             </AppBar>
 
-            <Drawer
-                variant="permanent"
-                className={classes.drawer}
-                anchor="left"
-                classes={{
-                    paper: classes.drawerPaper
-                }}
-            >
-                <div>
-                    <Divider />
-                    <List>
-                        {
-                            list.map((item) => {
-                                return (
-                                    <ListItemButton
-                                        Key={item.text}
-                                        onClick={() => history.push(item.path)}
-                                        selected={location.pathname === item.path ? true : false}
-                                    >
-                                        <ListItemIcon>{item.icon}</ListItemIcon>
-                                        <ListItemText>{item.text}</ListItemText>
-                                    </ListItemButton>
-                                );
-                            })
-                        }
-                    </List>
-                </div>
-            </Drawer>
+            {
+                !mobile
+                &&
+                <Drawer
+                    variant="permanent"
+                    className={classes.drawer}
+                    anchor="left"
+                    classes={{
+                        paper: classes.drawerPaper
+                    }}
+                >
+                    <div>
+                        <Toolbar />
+                        <Divider />
+                        <List>
+                            {
+                                list.map((item) => {
+                                    return (
+                                        <ListItemButton
+                                            Key={item.text}
+                                            onClick={() => history.push(item.path)}
+                                            selected={location.pathname === item.path ? true : false}
+                                        >
+                                            <ListItemIcon>{item.icon}</ListItemIcon>
+                                            <ListItemText>{item.text}</ListItemText>
+                                        </ListItemButton>
+                                    );
+                                })
+                            }
+                        </List>
+                    </div>
+                </Drawer>
+            }
 
             <div className={classes.layout}>
-                <div className={classes.toolbar}>
+                <div className={classes.toolbar} />
 
-                </div>
-                {children}
+                {
+                    mobile
+                    ?
+                    tab === 0
+                    ?
+                    <Home />
+                    :
+                    <Create />
+                    :
+                    children
+                }
+
+                <div className={classes.toolbar} />
+
+                {
+                    mobile
+                    &&
+                    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={0}>
+                        <BottomNavigation
+                        showLabels
+                        value={tab}
+                        onChange={(event, newValue) => {
+                            setTab(newValue);
+                            console.log(newValue);
+                        }}
+                        >
+                        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+                        <BottomNavigationAction label="Add" icon={<Add />} />
+                        </BottomNavigation>
+                    </Paper>
+                }
             </div>
         </div>
     );
