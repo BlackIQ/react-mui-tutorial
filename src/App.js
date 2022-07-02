fe99999baa3aed23252669b593741df09df22d5d
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-02 07:45:20 +0430

    Adding dynamic color

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Axios from "axios";
 
 import {
   createTheme,
   ThemeProvider
 } from "@mui/material/styles";
 
+import ColorPick from './components/color';
 import Create from './components/create';
 import Home from './components/home';
 
 import Layout from './components/layout';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#ff0000"
+const theme = createTheme((themeColor) => {
+  return {
+    palette: {
+      mode: 'light',
+      primary: {
+        main: themeColor
+      },
     },
-  },
-  typography: {
-    fontFamily: "Arima",
-    fontWeightLight: 200,
-    fontWeightRegular: 300,
-    fontWeightMedium: 400,
-    fontWeightBold: 600
-  }
+    typography: {
+      fontFamily: "Arima",
+      fontWeightLight: 200,
+      fontWeightRegular: 300,
+      fontWeightMedium: 400,
+      fontWeightBold: 600
+    }
+  };
 });
 
 function App() {
+  const [themeColor, setThemeColor] = useState('#ff0000');
+
+  useEffect(() => {
+    Axios.get('http://localhost:8000/theme')
+      .then((result) => {
+        const theme = result.data;
+
+        console.log(theme);
+        // setThemeColor(theme.color);
+      })
+      .catch((error) => console.log(error));
+  });
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme(themeColor)}>
       <Router>
         <Layout>
           <Switch>
@@ -36,6 +55,9 @@ function App() {
             </Route>
             <Route exact path='/create'>
               <Create />
+            </Route>
+            <Route exact path='/color'>
+              <ColorPick />
             </Route>
           </Switch>
         </Layout>
